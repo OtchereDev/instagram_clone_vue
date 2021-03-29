8f21984b7d3f74bce22388954505dd8b24d83a44
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-29 11:10:23 +0000

    added component for signing up

---
+++ src/components/SignUp.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class='w-11/12 mx-auto'>
+    <form >
+        <input type="email" placeholder="Email or username" class="py-3 px-2 placeholder-gray-500 rounded-lg bg-gray-900 outline-none text-gray-300 focus:bg-gray-700 w-full">
+
+        <input type="text" name="" id="" placeholder="Full name" class="py-3 px-2 my-3 placeholder-gray-500 rounded-lg bg-gray-900 outline-none text-gray-300 focus:bg-gray-700 w-full">
+        
+        <input type="text" name="" id="" placeholder="Username" class="py-3 px-2 mb-3 placeholder-gray-500 rounded-lg bg-gray-900 outline-none text-gray-300 focus:bg-gray-700 w-full">
+
+        <input type="password" placeholder='Password' class="py-3 px-2 mb-3 placeholder-gray-500 rounded-lg bg-gray-900 outline-none text-gray-300 focus:bg-gray-700 w-full">
+        
+
+        <div>
+            <button class="w-full py-3 focus:outline-none text-center bg-blue-600 text-gray-200 rounded-lg">
+                Sign up
+            </button>
+        </div>
+
+    </form>
+      </div>
+      
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
