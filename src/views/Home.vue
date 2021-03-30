a4e96a0461c606e16c2c629e8f8459f9e1000733
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-30 12:30:01 +0000

    almost finished with home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-screen relative overflow-x-hidden home">
+    <nav class='mx-5 flex justify-between items-center py-5 z-50'>
+      <div class="w-4/12">
+        <img :src="require('@/assets/logo_white.svg')" alt="">
+      </div>
+      <div class="w-4/12 flex justify-between items-center">
+        <div><ion-icon name="add-circle-outline" class="w-9 mr-3 text-4xl text-gray-100 font-bold cursor-pointer"></ion-icon></div>
+        <div><ion-icon name="heart-outline" class="w-9 mr-3 text-4xl text-gray-100 font-bold cursor-pointer"></ion-icon></div>
+        <div><ion-icon name="paper-plane-outline" class="w-9 mr-3 text-4xl text-gray-100 font-bold cursor-pointer"></ion-icon></div>
+      </div>
+    </nav>
+
+    <div class="relative">
+
+        <div class="flex overflow-x-auto stories">
+          
+          <Story v-for="i in 13" :key='i' />
+        </div>
+        
+        <section  class="pt-2">
+          <Post v-for="i in 6" :key='i' />
+        </section>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Post from '@/components/Post.vue'
+import Story from '@/components/Story.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Post,Story
   }
 }
 </script>
+
+<style >
+  .story{
+    min-width: 3.5rem;
+  }
+  .stories::-webkit-scrollbar{
+    width:0
+  }
+
+  .home::-webkit-scrollbar{
+    width: 0px
+  }
+</style>
